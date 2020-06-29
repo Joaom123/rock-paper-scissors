b1bc4425dc440a74fd0ce1f0d263b7f9b15e31c4
Joaom123 <joaomarcus12769@gmail.com>
2020-06-29 00:29:53 -0300

    commit inicial

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+import RockPaperScissorsGame from "./pages/RockPaperScissorsGame";
+
+function App() {
+  return (
+    <RockPaperScissorsGame />
+  );
+}
+
+export default App;
---
+++ src/pages/RockPaperScissorsGame/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function RockPaperScissorsGame({}) {
+    return null;
+}
+
+export default RockPaperScissorsGame;
