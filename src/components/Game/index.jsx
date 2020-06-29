2a91b75326c0fc2cbfba69580a18d998a8bb59c0
Joaom123 <joaomarcus12769@gmail.com>
2020-06-29 02:12:20 -0300

    definindo componentes

---
+++ src/components/Game/index.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./index.css";
+
+function Game({}) {
+    return (
+        <section className="game">
+
+        </section>
+    );
+}
+
+export default Game;
--- src/pages/RockPaperScissorsGame/index.jsx
+++ src/pages/RockPaperScissorsGame/index.jsx
@@ -1,7 +1,36 @@
-import React from "react";
+import React, {Fragment} from "react";
+import {Game} from "../../components";
 
 function RockPaperScissorsGame({}) {
-    return null;
+
+    return (
+        <Fragment>
+            <Head />
+            <Game />
+            <Rules />
+        </Fragment>
+    );
 }
 
 export default RockPaperScissorsGame;
+
+function Head({}) {
+    return (
+        <article className="">
+            <header>
+                <h1>ROCK PAPER SCISSORS</h1>
+            </header>
+            <div className="score">
+
+            </div>
+        </article>
+    );
+}
+
+function Rules({}) {
+    return (
+        <section className="rules">
+
+        </section>
+    );
+}
