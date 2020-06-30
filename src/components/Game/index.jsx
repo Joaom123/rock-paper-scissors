4b9fc86cdb1dd6c62b8709a4dff9c0bd171af79a
Joaom123 <joaomarcus12769@gmail.com>
2020-06-30 09:43:20 -0300

    trocando texto para logo

--- src/components/Game/index.jsx
+++ src/components/Game/index.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import "./index.css";
+import {BgTriangle} from "../../assets";
 
 function Game({}) {
     return (
         <section className="game">
-
+            <BgTriangle className="game__" />
         </section>
     );
 }
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 import "./index.css";
+import {Logo} from "../../assets";
 
 function Header({score}) {
     return (
         <article className="header">
-            <header className="header__title">
-                <h1>ROCK PAPER SCISSORS</h1>
-            </header>
+            <Logo className="header__title" />
             <div className="headerScore">
                 <span className="headerScore__title">SCORE</span>
                 <span className="headerScore__score">{score}</span>
