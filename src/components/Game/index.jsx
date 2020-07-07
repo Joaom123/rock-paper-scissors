91536812f555d048eb1002930ef437757890e194
Joaom123 <joaomarcus12769@gmail.com>
2020-07-06 23:31:52 -0300

    começando escolha da mão

--- src/components/Game/index.jsx
+++ src/components/Game/index.jsx
@@ -3,24 +3,31 @@ import "./index.css";
 import {IconRock, IconPaper, IconScissors} from "../../assets";
 import bgTriangle from "../../assets/img/bg-triangle.svg";
 
-function Game({}) {
+function Game({onClick, selectedType}) {
+    if(selectedType)
+        return null;
+
+    return <InitialGame onClick={onClick}/>;
+}
+
+export default Game;
+
+function InitialGame({onClick}) {
     return (
         <section
             className="game"
             style={{backgroundImage: `url(${bgTriangle})`}}
         >
-            <Hand type="paper" />
-            <Hand type="scissors" />
-            <Hand type="rock" />
+            <Hand type="paper" onClick={onClick}/>
+            <Hand type="scissors" onClick={onClick}/>
+            <Hand type="rock" onClick={onClick}/>
         </section>
     );
 }
 
-export default Game;
-
-function Hand({type}) {
+function Hand({type, onClick}) {
     return(
-        <div className={`game__hand game__hand--${type}`}>
+        <div className={`game__hand game__hand--${type}`} onClick={() => onClick(type)}>
             <div className={`game__handIcon game__handIcon--${type}`}>
                 <Icon type={type} />
             </div>
--- src/pages/RockPaperScissorsGame/index.jsx
+++ src/pages/RockPaperScissorsGame/index.jsx
@@ -4,11 +4,14 @@ import "./index.css";
 
 function RockPaperScissorsGame({}) {
     const [score, setScore] = useState(0);
+    const [selectedType, setSelectedType] = useState("");
+
+    const handleClickOnHand = type => setSelectedType(type);
 
     return (
         <div className="container">
             <Header score={score}/>
-            <Game />
+            <Game onClick={handleClickOnHand} selectedType={selectedType}/>
             <Rules />
         </div>
     );
