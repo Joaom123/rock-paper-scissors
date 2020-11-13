c1b0959d48ec1b4dbbd525905fd518525439ba29
João Marcus <joaomarcus12769@gmail.com>
2020-11-13 03:46:33 -0300

    começando a lógica do jogo

---
+++ src/service/gameLogic.js
@@ -0,0 +1,9 @@
+// Returns from the perspective of the human player:
+// won -> 1
+// tied -> 0
+// lost -> -1
+function game({playerHand, machineHand}) {
+    throw new Error("");
+}
+
+export default game;
