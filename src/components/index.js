a85ebf2dbcb3826c30af04015688514ec99d1625
João Marcus <joaomarcus12769@gmail.com>
2022-01-28 22:48:32 -0300

    prettier

--- src/components/index.js
+++ src/components/index.js
@@ -1,11 +1,11 @@
-import Game from "./Game";
-import Rules from "./Rules";
-import Header from "./Header";
-import Hand from "./Hand";
+import Game from './Game';
+import Rules from './Rules';
+import Header from './Header';
+import Hand from './Hand';
 
 export {
-    Game,
-    Rules,
-    Header,
-    Hand
+  Game,
+  Rules,
+  Header,
+  Hand,
 };
--- src/service/gameLogic.js
+++ src/service/gameLogic.js
@@ -2,8 +2,8 @@
 // won -> 1
 // tied -> 0
 // lost -> -1
-function game({playerHand, machineHand}) {
-    return 1
+function game({ playerHand, machineHand }) {
+  return 1;
 }
 
 export default game;
