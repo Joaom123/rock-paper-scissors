81a74daf87679bb8ddd829cc98372717d6bf22d6
João Marcus <joaomarcus12769@gmail.com>
2022-01-30 11:16:06 -0300

    test

--- src/components/Header/index.test.js
+++ src/components/Header/index.test.js
@@ -5,23 +5,27 @@ import { render, screen } from '@testing-library/react'
 describe('Header', () => {
 	it('should render correctly with Logo and Score', () => {
 		render(<Header score={0} />)
+
 		expect(screen.queryByTestId('header')).toBeTruthy()
 	})
 
 	it('should render logo', function () {
 		render(<Header score={0} />)
+
 		expect(screen.queryByTestId('header-logo')).toHaveTextContent('logo.svg')
 	})
 
 	it('should have initial score equals zero', function () {
 		render(<Header score={0} />)
+
 		expect(screen.getByTestId('header-score-value')).toHaveTextContent('0')
 	})
 
 	it('should change value of score if props change', function () {
 		const { rerender } = render(<Header score={0} />)
-		expect(screen.getByTestId('header-score-value')).toHaveTextContent('0')
+
 		rerender(<Header score={1} />)
+
 		expect(screen.getByTestId('header-score-value')).toHaveTextContent('1')
 	})
 })
